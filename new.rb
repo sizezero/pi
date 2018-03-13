666b8a10a868cd91d6ed33e4aaf1aea5d657b9e9
Robert Kleemann <robert@kleemann.org>
2018-03-12 19:36:36 -0700

    easy way to copy template file

---
+++ new.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+i = 1
+while true do
+  filename = "try-#{'%05d' % i}.txt"
+  if !File.exist?(filename)
+    FileUtils.cp("template.txt", filename)
+    exit
+  end
+  i += 1
+end
+  
